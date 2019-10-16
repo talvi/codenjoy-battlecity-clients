76ed763e6ff686a9405e8ceecaf446052529b4b6
apostol <dev@dpankratov.ru>
2019-10-16 21:07:22 +0400

    +.Net, +C++

---
+++ Cpp/CodeBattleCpp/CodeBattleCpp.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+#include <random>
+
+#include "GameClientBattlecity.h"
+#include "BattlecityBlocks.h"
+#include "Point.h"
+
+
+void main()
+{
+	srand(time(0));
+	GameClientBattlecity *gcb = new GameClientBattlecity("http://dojorena.io/codenjoy-contest/board/player/0?code=000000000000&gameName=battlecity");
+	
+	gcb->Run([&]()
+	{
+		bool done = false;
+
+		gcb->Right(FireAction::AfterTurn);
+		done = true;
+
+		if (done == false)
+			gcb->Blank();
+
+	});
+
+	getchar();
+}
---
+++ Net/CodeBattleNet/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using CodeBattleNetLibrary;
+
+namespace CodeBattleNet
+{
+    internal static class Program
+    {
+        private static void Main()
+        {
+            var gcb = new GameClientBattlecity("http://dojorena.io/codenjoy-contest/board/player/0?code=000000000000");
+            gcb.Run(() =>
+            {
+                Move(gcb);
+            });
+            Console.Read();
+        }
+
+        private static void Move(GameClientBattlecity gcb)
+        {
+            var r = new Random();
+            var done = false;
+
+            switch (r.Next(5))
+            {
+                case 0:
+                    if (!gcb.IsBarrierAt(gcb.PlayerX, gcb.PlayerY - 1))
+                    {
+                        gcb.SendActions(gcb.Up());
+                        done = true;
+                    }
+                    break;
+                case 1:
+                    if (!gcb.IsBarrierAt(gcb.PlayerX + 1, gcb.PlayerY))
+                    {
+                        gcb.SendActions(gcb.Right());
+                        done = true;
+                    }
+                    break;
+                case 2:
+                    if (!gcb.IsBarrierAt(gcb.PlayerX, gcb.PlayerY + 1))
+                    {
+                        gcb.SendActions(gcb.Down());
+                        done = true;
+                    }
+                    break;
+                case 3:
+                    if (!gcb.IsBarrierAt(gcb.PlayerX - 1, gcb.PlayerY))
+                    {
+                        gcb.SendActions(gcb.Left());
+                        done = true;
+                    }
+                    break;
+                case 4:
+                    gcb.SendActions(gcb.Act());
+                    done = true;
+                    break;
+            }
+            if (done == false)
+                gcb.SendActions(gcb.Blank());
+        }
+    }
+}
